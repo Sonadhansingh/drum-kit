8f6f40825c8216c239a8f9a99efaeb9c4606744d
Sona Dhansingh <126173016+Sonadhansingh@users.noreply.github.com>
2024-03-16 20:41:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+
+var buttonNumber = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<buttonNumber;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "1":
+                var sound1= new Audio("audio/sound1.mp3");
+                sound1.play();
+                break;
+
+            case "2":
+                var sound1= new Audio("audio/sound2.mp3");
+                sound1.play();
+                break;
+
+            case "3":
+                var sound1= new Audio("audio/sound3.mp3");
+                sound1.play();
+                break;
+            
+            case "4":
+                var sound1= new Audio("audio/sound6.mp3");
+                sound1.play();
+                break;
+
+            case "5":
+                var sound1= new Audio("audio/sound7.mp3");
+                sound1.play();
+                break;
+
+            case "6":
+                var sound1= new Audio("audio/sound8.mp3");
+                sound1.play();
+                break;
+    
+            case "7":
+                var sound1= new Audio("audio/sound5.mp3");
+                sound1.play();
+                break;
+    
+            case "8":
+                var sound1= new Audio("audio/sound4.mp3");
+                sound1.play();
+                break;
+                
+            case "9":
+                var sound1= new Audio("audio/sound9.mp3");
+                sound1.play();
+                break;
+    
+                
+
+            default:
+                break;
+        }
+
+    });
+}
